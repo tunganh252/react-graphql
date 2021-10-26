510feb867f22e1a748d3e4e1b2233be5c6a216ae
tunganh252 <tunganhnguyen2502.com>
2021-10-27 00:06:05 +0700

    [create] - server graphql + postgreSQL

---
+++ server/src/index.ts
@@ -0,0 +1,46 @@
+require("dotenv").config();
+import "reflect-metadata";
+import express from "express";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
+import { Post } from "./entities/Post";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/user";
+import { HelloResolver } from "./resolvers/hello";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const main = async () => {
+  await createConnection({
+    type: "postgres",
+    database: "fsNta",
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    logging: true,
+    synchronize: true,
+    entities: [User, Post],
+  });
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, UserResolver],
+      validate: false,
+    }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () =>
+    console.log(
+      `Server started on port ${PORT}. GrapQL server started on localhost:${PORT}${apolloServer.graphqlPath}`
+    )
+  );
+};
+
+main().catch((error) => console.log(error));
---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,31 @@
+import { User } from "../entities/User";
+import { Arg, Mutation, Resolver } from "type-graphql";
+import argon2 from "argon2";
+
+@Resolver()
+export class UserResolver {
+  @Mutation((_returns) => User, { nullable: true })
+  async register(
+    @Arg("email") email: string,
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ): Promise<User | null> {
+    try {
+      const existingUser = await User.findOne({ username });
+      if (existingUser) return null;
+
+      const hashPassword = await argon2.hash(password);
+
+      const newUser = User.create({
+        email,
+        username,
+        password: hashPassword,
+      });
+
+      return await User.save(newUser);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
